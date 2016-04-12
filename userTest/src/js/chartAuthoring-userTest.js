4b3feea350997139f0ee7f8e3037daf785600b34
Alan Harnum <aharnum@ocadu.ca>
2016-04-12 15:58:45 -0400

    NOJIRA: add randomized play order

--- userTest/src/js/chartAuthoring-userTest.js
+++ userTest/src/js/chartAuthoring-userTest.js
@@ -128,4 +128,22 @@
         initialData: smartPhoneData
     });
 
+
+    // From http://stackoverflow.com/a/1533945
+    $.fn.randomize = function (childElem) {
+        return this.each(function () {
+            var $this = $(this);
+            var elems = $this.children(childElem);
+
+            elems.sort(function () { return (Math.round(Math.random()) - 0.5); });
+
+            $this.detach(childElem);
+
+            for (var i = 0; i < elems.length; i++) {
+                $this.append(elems[i]);
+            }
+        });
+    };
+
+
 })(jQuery, fluid);
